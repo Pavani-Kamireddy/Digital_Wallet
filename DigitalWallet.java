36696f6f693d17f80d978dd7f1ba60abfa26fb18
Pavani-Kamireddy <pavanikamireddy12@gmail.com>
2015-09-16 12:36:26 +0530

    money added to wallet, and can make a payment from wallet.

--- DigitalWallet.java
+++ DigitalWallet.java
@@ -1,20 +1,46 @@
 public class DigitalWallet {
   
+  String name;
+  static double bal_in_wallet=0.0;
   /* Store all the Add Money, Pay and Reward transactions as transaction objects */
 
   /* implement all the methods given below */
 
   /* Default constructor */
-  public DigitalWallet(String name ) {}
+  public DigitalWallet(String name ) {
+    this.name=name;
+  }
 
   /* Add money to the wallet */
-  public boolean addMoney(Transaction tx) { return false; }
+  public boolean addMoney(Transaction tx) { 
+    if(tx.getAmount()<0||tx.getAmount()>5000) {
+      return false;
+    }
+    else { 
+      bal_in_wallet=bal_in_wallet+tx.getAmount();
+      return true;
+    }
+  }
 
   /* Return the balance in the wallet */
-  public double getBalance() {return 0.0;}
+  public double getBalance() {
+    return return bal_in_wallet;;
+    
+  }
 
   /* Make a payment */
-  public boolean pay(Transaction tx) { return false; }
+  public boolean pay(Transaction tx) { 
+    if(getBalance()<tx.getAmount())
+      return false; 
+    else {
+      bal_in_wallet=bal_in_wallet-tx.getAmount();
+      if(tx.getType().equals("Payment")) {
+          int temp=(int)tx.getAmount()/100;
+          bal_in_wallet=bal_in_wallet+temp*10;
+      }
+      return true;
+    }
+  }
 
   /* print statement */
   public void printStatement() {}

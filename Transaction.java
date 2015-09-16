fcbf52d45739025bc3b134000a1a42dad4ccda8d
Pavani-Kamireddy <pavanikamireddy12@gmail.com>
2015-09-16 11:26:52 +0530

    Update Transaction.java
    set and get methods

--- Transaction.java
+++ Transaction.java
@@ -1,9 +1,43 @@
 import java.util.Date;
 
 public class Transaction {
-  public void setType(String type) {}
-  public void setAmount(double amount) {}
-  public void setDate(Date date) {}
-  public void setDescription(String desc) {}
-  public void setMerchant(String merchant) {}
+	String type;
+	double amount;
+  String merchant;
+  String str;
+  String process;
+  String desc;
+  public void setType(String type) {
+  	this.type=type;
+  }
+  public String getType() {
+    return type;
+  }
+  public void setAmount(double amount) {
+  	this.amount=amount;
+  }
+  public double getAmount() {
+    return amount;
+  }
+  
+  public void setDate(Date date) {
+  	date=new Date();
+  	str = String.format("Current Date/Time : %tc", date );
+	  //System.out.printf(str);
+  }
+  public String getDate() {
+    return str;
+  }
+  public void setDescription(String desc) {
+    this.desc=desc;
+  }
+  public String getDescription(){
+    return desc;
+  }
+  public void setMerchant(String merchant) {
+    this.merchant=merchant;
+  }
+  public String getMerchant(){
+    return merchant;
+  }
 }

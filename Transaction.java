35aed4239160383dc37c817e42f5b2bae3e320d8
Pavani-Kamireddy <pavanikamireddy12@gmail.com>
2015-09-16 10:37:39 +0530

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Transaction {
+  public void setType(String type) {}
+  public void setAmount(double amount) {}
+  public void setDate(Date date) {}
+  public void setDescription(String desc) {}
+  public void setMerchant(String merchant) {}
+}
